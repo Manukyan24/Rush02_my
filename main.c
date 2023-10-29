ef34f285eb956ffb7b3ab1455008d1c34978306a
Vladimir Manukyan <vladimir_manukyan@yahoo.com>
2023-10-29 11:17:15 +0400

    working prototype with some bug

--- main.c
+++ main.c
@@ -2,53 +2,63 @@
 #include <unistd.h>
 #include <stdio.h>
 
-char **get_pre_parsed_array(char *filename)
+typedef struct array
 {
+	char **array;
+	int size;
+} array;
+
+
+array *get_pre_parsed_array(char *filename)
+{
+	array *pre_parsed_array = (array*)malloc(sizeof(array));
 	int fd;
 	char *temp;
-	char	**pre_parsed_array;
-	int dict_len;
 	int i;
 	int j;
 	int row_len;
 
-	dict_len = 0;
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return NULL;
+	pre_parsed_array -> size  = 0;	
 	while (get_next_line(fd) != NULL)
-		dict_len++;
-	dict_len--;
+		(pre_parsed_array -> size)++;
 	if (close(fd) == -1)
 		return NULL;
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return NULL;
-	pre_parsed_array = (char **)malloc(dict_len * sizeof(char*));
+	pre_parsed_array -> array = (char **)malloc((pre_parsed_array -> size) * sizeof(char*));
+	if ((pre_parsed_array -> array) == NULL)
+		return NULL;
 	i = 0;
-	while (i < dict_len  && (temp = get_next_line(fd)) != NULL)
+	while (i < (pre_parsed_array -> size)  && (temp = get_next_line(fd)) != NULL)
 	{
 		row_len = 0;
 		while (temp[row_len] != '\0')
 			row_len++;
-		pre_parsed_array[i] = (char*)malloc(row_len * sizeof(char));
+		(pre_parsed_array -> array)[i] = (char*)malloc(row_len * sizeof(char));
+		if ((pre_parsed_array -> array)[i] == NULL)
+			return NULL;
 		j = 0;
 		while (j < row_len)
 		{
-			pre_parsed_array[i][j] = temp[j];
+			(pre_parsed_array -> array)[i][j] = temp[j];
 			j++;
 		}
-		pre_parsed_array[i][j] = '\0';
+		(pre_parsed_array -> array)[i][j] = '\0';
 		i++;
 	}
 	return (pre_parsed_array);
-}
 
+	
+}
 
 int main()
 {
-	char **temp = get_pre_parsed_array("numbers.dict");
 	int i;
-	for (i = 0; i < 40; i++)
-		printf("%s", temp[i]);
+	array* a = get_pre_parsed_array("numbers.dict");
+	for (i = 0; i < a -> size; i++)
+		printf("%s", (a -> array)[i]);
 }	
